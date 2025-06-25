490bfaf7e89ff9bedadd23c8c0a1548315483103
Tiny <119753207+xobby@users.noreply.github.com>
2025-06-25 14:26:49 +0200

    Delete app directory

--- app/components/card.jsx
+++
@@ -1,8 +0,0 @@
-export const Card = ({ children }) => {
-
-	return (
-		<div className="overflow-hidden relative duration-700 border rounded-xl hover:bg-zinc-800/10 group md:gap-8 hover:border-zinc-400/50 border-zinc-600">
-			{children}
-		</div>
-	);
-};
